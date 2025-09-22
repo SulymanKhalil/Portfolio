be1fa465a6b909503ae68fc5bb8596e9caf4bc86
Sulyman Khalil <sulymankhalil87@gmail.com>
2025-09-22 17:44:18 +0500

    Add GitHub Pages deploy config and homepage; switch to HashRouter for routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import HeroSection from "./Components/HeroSection";
 import About from "./Components/About";
