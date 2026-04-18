7835b76280352fee339191e7068efec82472247e
Sulyman Khalil <sulyman.hkalil@multiconnect.com.hk>
2026-04-18 12:22:03 +0500

    Updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,12 +2,12 @@ const Footer = () => {
   return (
     <div className="footer" data-aos="fade-up" data-aos-offset="150">
       <h1>Sulyman Khalil</h1>
-      <p>Frontend Developer | React.js Developer | Next.js Developer</p>
+      <p>Frontend Developer | React.js & Next.js | Video Streaming (WebRTC, FFmpeg)</p>
       <hr />
       <p className="copyright-line">
         Copyright © 2025 | Sulyman Khalil <br /> All rights reserved.
       </p>
-      <p className="last-updated">Last updated: 02 April, 2026</p>
+      <p className="last-updated">Last updated: 18 April, 2026</p>
     </div>
   );
 };
