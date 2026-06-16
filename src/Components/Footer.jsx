94f2b9bf6581122eaadd20fb9ef2bff01f3fc64f
Sulyman Khalil <Sulymankhalil.dev@gmail.com>
2026-06-17 00:40:50 +0500

    Updated last updated date

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <p className="copyright-line">
         Copyright © 2025 | Sulyman Khalil <br /> All rights reserved.
       </p>
-      <p className="last-updated">Last updated: 18 May, 2026</p>
+      <p className="last-updated">Last updated: 17 June, 2026</p>
     </div>
   );
 };
